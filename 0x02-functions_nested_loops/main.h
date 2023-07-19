976a39a3205e12708325b857618db23b1eda3419
Goldenm09 <iamgoldengirl2@gmail.com>
2023-07-18 19:35:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Other function prototypes you may already have */
-/* ... */
-
 /* Function prototype for _putchar */
 int _putchar(char c);
 
-/* Function prototype for _islower */
-int _islower(int c);
-
-/* Function prototype for _isalpha */
-int _isalpha(int c);
+/* Function prototype for print_sign */
+int print_sign(int n);
 
 #endif /* MAIN_H */
